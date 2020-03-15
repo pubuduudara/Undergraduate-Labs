01bdf731a67622ab5b8d78d45669f9c04b56726c
pubuduudara <pubuduudara7@gmail.com>
2020-03-15 14:00:14 +0530

    update

---
+++ Semester 6/CO527/Lab01/Lab 01 - Resources-20200312/Resources/load_salaries1.sql
@@ -0,0 +1,2 @@
+<br />
+<b>Warning</b>:  readfile(/webdata/moodle/engmoodle/filedir/5b/94/5b94f23b2a58e155c7977926c979f4b9310b9d10): failed to open stream: No such file or directory in <b>/usr/local/www/feelsold.pdn.ac.lk/lib/filelib.php</b> on line <b>1854</b><br />
